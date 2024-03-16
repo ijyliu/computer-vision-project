4d226f8925e2a86608929c10e7152ec13926ae2b
Isaac Liu <ijyliu@scf.berkeley.edu>
2024-03-15 21:16:03 -0700

    copying working

--- Code/Rclone_2_Way_Copy.sh
+++ Code/Rclone_2_Way_Copy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 #SBATCH --job-name=Rclone_2_Way_Copy
+#SBATCH --partition=lowmem
+#SBATCH --output=Rclone_2_Way_Copy.out
 
-# Suppress output
-#SBATCH --output=/dev/null
+echo "Making directory"
 
 # Make directory if needed
 mkdir -p "~/Box/INFO 290T Project"
 
-# Load rclone module
-module load rclone
+echo "Beginning Push"
 
 # Push to remote/online version
-rclone copy "~/Box/INFO 290T Project" "Box:INFO 290T Project" --update
+rclone copy ~/Box/"INFO 290T Project" "Box:INFO 290T Project" --update
+
+echo "Beginning Pull"
+
 # Pull from remote/online version
-rclone copy "Box:INFO 290T Project" "~/Box/INFO 290T Project" --update
+rclone copy "Box:INFO 290T Project" ~/Box/"INFO 290T Project" --update
