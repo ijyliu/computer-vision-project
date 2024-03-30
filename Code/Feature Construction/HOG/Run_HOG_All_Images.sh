c9b52877de61d8d2a06caf5c2fcab157849dc7d7
Isaac Liu <ijyliu@uchicago.edu>
2024-03-30 14:13:29 -0700

    HOG sample run ready

---
+++ Code/Feature Construction/HOG/Run_HOG_All_Images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=Run_HOG_All_Images
+#SBATCH --output=Run_HOG_All_Images.out
+#SBATCH --partition=jsteinhardt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+
+# Timing
+# Reset the SECONDS variable
+SECONDS=0
+
+# Run script
+python Run_HOG_All_Images.py
+
+# Calculate time in minutes
+elapsed_minutes=$((SECONDS / 60))
+# Print the time elapsed in minutes
+echo "Time elapsed: $elapsed_minutes minute(s)."
