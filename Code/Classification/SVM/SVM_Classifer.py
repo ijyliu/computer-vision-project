f24788954f8cc825d6c8604c188b521212c56e79
Gaurav Sharan Srivastava <158238900+CalBlitz@users.noreply.github.com>
2024-04-07 21:57:58 -0700

    Update SVM_Classifer.py

--- Code/Classification/SVM/SVM_Classifer.py
+++ Code/Classification/SVM/SVM_Classifer.py
@@ -7,7 +7,7 @@ import time
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 # Prepare Training Data
 def combine_directory_parquets(directory_path):
