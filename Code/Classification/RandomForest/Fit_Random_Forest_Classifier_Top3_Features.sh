15b4e32a04c7a3b3ab572ff8b10d9a273b6cf9a5
Isaac Liu <ijyliu@uchicago.edu>
2024-04-14 17:20:23 -0700

    top3 scripts

---
+++ Code/Classification/RandomForest/Fit_Random_Forest_Classifier_Top3_Features.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=Fit_Random_Forest_Classifier_Top3_Features
+#SBATCH --output=Fit_Random_Forest_Classifier_Top3_Features.out
+#SBATCH --partition=epurdom
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+
+# Timing
+# Reset the SECONDS variable
+SECONDS=0
+
+echo "Starting Job"
+
+# Execute the notebook
+jupyter nbconvert --to notebook --execute --inplace Fit_Random_Forest_Classifier_Top3_Features.ipynb
+
+echo "Completed Job"
+
+# Calculate time in minutes
+elapsed_minutes=$((SECONDS / 60))
+# Print the time elapsed in minutes
+echo "Time elapsed: $elapsed_minutes minute(s)."
