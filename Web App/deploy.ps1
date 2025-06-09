3d699c0240f3db8f71a84416004bc7bd2410b95c
Isaac Liu <ijyliu@gmail.com>
2025-06-08 23:11:13 -0400

    update favicon and fix deployment script

--- Web App/deploy.ps1
+++ Web App/deploy.ps1
@@ -1,3 +1,3 @@
-docker build -t gcr.io/cv-web-app/cv-web-app:latest .
-docker push gcr.io/cv-web-app/cv-web-app:latest
-gcloud run deploy cv-web-app --image gcr.io/cv-web-app/cv-web-app:latest --region us-central1 --allow-unauthenticated --memory 4G --cpu 1 --max-instances 2
+docker build -t gcr.io/cv-web-app-425302/cv-web-app:latest .
+docker push gcr.io/cv-web-app-425302/cv-web-app:latest
+gcloud run deploy cv-web-app --image gcr.io/cv-web-app-425302/cv-web-app:latest --region us-central1 --allow-unauthenticated --memory 4G --cpu 1 --max-instances 2
